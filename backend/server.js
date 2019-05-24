95708b7a0d44017be0bea095952f62e606737120
Michael HALIT <mhalit@e1r7p5.42.fr>
2019-05-24 23:15:02 +0200

    db

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const cors = require("cors")
+
+
+const config = require('../config/config');
+
+
+
+
 const port = process.env.PORT || 7777;
 app.use(morgan('dev')); // log requests to the console
 app.use(cors())
@@ -19,16 +26,28 @@ const allowCrossDomain = (req, res, next) => {
   next();
 }
 
-const path = require('path')
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'tracker/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/tracker/build/index.html'))
-})
+// const path = require('path')
+// // Serve static files from the React frontend app
+// app.use(express.static(path.join(__dirname, 'tracker/build')))
+// // Anything that doesn't match the above, send back index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/tracker/build/index.html'))
+// })
 
 
 app.use(allowCrossDomain);
+
+
+// MongoDb
+mongoose.connect(`mongodb+srv://root:${config.mongo.password}@hubguardian-13shh.mongodb.net/test`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("DB connection alive");
+});
 
 // Router
 const router = require('./router/index');
