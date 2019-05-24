817330c486198c24b65437ba649701f8f3c58403
Michael HALIT <mhalit@e1r7p5.42.fr>
2019-05-24 22:40:05 +0200

    remove junk

--- front/src/App.js
+++ front/src/App.js
@@ -12,8 +12,8 @@ import {
   Grid,
   GridRow
 } from 'semantic-ui-react'
-import TradingViewWidget, { Themes } from 'react-tradingview-widget'
-import marketDataService from './services/market-service'
+
+// import marketDataService from './services/market-service'
 
 import 'semantic-ui-css/semantic.min.css'
 import 'react-semantic-toasts/styles/react-semantic-alert.css';
@@ -24,22 +24,14 @@ function App() {
   const [position, setPosition] = useState([])
   const [loading, setLoading] = useState(false)
 
-
   useEffect(() => {
     const fetchData = async () => {
-      const result = await marketDataService.fetchPosition()
+      // const result = await marketDataService.fetchPosition()
       setLoading(true)
     }
     fetchData()
 
-    toast(
-      {
-          title: 'Info Toast',
-          description: <p>This is a Semantic UI toast</p>
-      },
-      () => console.log('toast closed'),
-      () => console.log('toast clicked')
-  );
+    
   }, [])
   return (
     <div className="App">
@@ -47,13 +39,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         The wizard is running...
         <Divider />
-        {loading && <BalanceComponent {...{ balance }} />}
-        <Divider />
-        {loading && <OrderComponent {...{ bids, orders }} />}
-        {loading && <ProfitComponent {...{ balance, position, bids, orders }} />}
+        {/* {loading && <BalanceComponent {...{ balance }} />} */}
 
-        <Divider />
-        {loading && <PositionComponent {...{ position }} />}
       </header>
     </div>
   )
